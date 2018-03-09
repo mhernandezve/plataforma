892569926b34e76d6153f1b30ae8d5faf57b202a
Carla Suarez <carlast22@gmail.com>
2018-03-08 19:17:15 -0500

    @jreyes33 #25 - Agrega clusters para visualización de datos en el mapa

--- web/themes/vivas/static/js/map.js
+++ web/themes/vivas/static/js/map.js
@@ -4,3 +4,23 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   minZoom: 6,
   maxZoom: 15
 }).addTo(map)
+
+var coordinates = [
+  [0.011944, -78.431389],
+  [-0.002222, -78.455833],
+  [-2.18093, -79.877929],
+  [-2.194061, -79.879874],
+  [-2.1890881,-79.9362873],
+  [-2.9014, -79.00679],
+  [-3.986703, -79.198936],
+  [-0.457212, -90.274332],
+  [-1.2975, -90.434167]
+]
+
+var clusterGroup = L.markerClusterGroup()
+
+coordinates.forEach(function(coordinate) {
+  L.marker(coordinate).addTo(clusterGroup)
+})
+
+clusterGroup.addTo(map)
