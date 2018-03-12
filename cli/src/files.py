e08c71f888794cbc6cc17d3f4cdfbfb0b2f690d5
Iván Pazmiño <iapazmino@gmail.com>
2018-03-12 11:55:25 -0500

    #3 - Verifica que exista el directorio y que hayan los archivos

--- cli/src/files.py
+++ cli/src/files.py
@@ -1,5 +1,13 @@
 import os
 import os.path as paths
 
-def exists(file):
-    return paths.isfile(file) and os.access(file, os.R_OK)
+def exists(path):
+    dir_exists = paths.isdir(path)
+    readable_file = paths.isfile(path) and os.access(path, os.R_OK)
+    return dir_exists or readable_file
+
+def is_empty(dir_path):
+    return os.listdir(dir_path) == []
+
+def remove(path):
+    os.remove(path)
--- cli/src/tests/test_loading_features.py
+++ cli/src/tests/test_loading_features.py
@@ -1,30 +1,12 @@
-from pytest import raises
+from context import load
 from context import files
 
 """
-Dado el directorio 'resources' que contien los archivos para cargar
+Dado el directorio 'resources' que contie los archivos para cargar
 Cuando ejecuto la carga
 Entonces obtengo el archivo resultado.csv en la misma carpeta
 """
 def test_load_files_in_directory():
     dir_path = './src/tests/resources'
-    load(dir_path)
+    load.load(dir_path)
     assert files.exists('./src/tests/resources/resultado.csv') == True
-
-def test_load_non_existent_file():
-    dir_path = './src/tests/not_existent'
-    expected_message = 'El archivo ./src/tests/not_existent/cedhu.xlsx no existe o no es posible leerlo'
-    with raises(IOError, message=expected_message) as file_not_found:
-        load(dir_path)
-    assert expected_message in str(file_not_found.value)
-
-def load(dir_path):
-    cedhu_file_name = '{dir}/cedhu.xlsx'.format(dir=dir_path)
-    if not files.exists(cedhu_file_name):
-        error_message = 'El archivo {file} no existe o no es posible leerlo'.format(file=cedhu_file_name)
-        raise IOError(error_message)
-
-    result_file_name = '{dir}/resultado.csv'.format(dir=dir_path)
-    with open(result_file_name, 'w+') as file:
-        file.write('\n')
-    return True
